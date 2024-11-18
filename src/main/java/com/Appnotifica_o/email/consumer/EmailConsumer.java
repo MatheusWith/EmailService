5ff3fd29654509e16640d58225b66a8311a95588
MatheusWith <matheuswithsilva@gmail.com>
2024-11-18 13:13:35 -0300

    Finalizadno EmailConsumer

--- src/main/java/com/Appnotifica_o/email/consumer/EmailConsumer.java
+++ src/main/java/com/Appnotifica_o/email/consumer/EmailConsumer.java
@@ -1,5 +1,7 @@
 package com.Appnotifica_o.email.consumer;
 
+import com.Appnotifica_o.email.dtos.DtoEmail;
+import com.Appnotifica_o.email.models.Email;
 import com.Appnotifica_o.email.service.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,17 +14,9 @@ public class EmailConsumer {
     private EmailService emailService;
 
     @RabbitListener(queues = "${broker.queue.notificacao.queue}")
-    public void listenerEmailQueue(@Payload DTOTeste dtoTeste) {
-        System.out.printf(dtoTeste.teste());
+    public void listenerEmailQueue(@Payload DtoEmail data) {
+        System.out.println(data.emailTo());
+        var email = new Email(data);
+        this.emailService.sendEmail(email);
     }
-    public record DTOTeste(String teste){
-    }
-
-
-//    @RabbitListener(queues = "${broker.queue.email.name}")
-//    public void listenerEmailQueue(@Payload DtoEmail data) {
-//        System.out.println(data.emailTo());
-//        var email = new Email(data);
-//        this.emailService.sendEmail(email);
-//    }
 }
